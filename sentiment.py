4453dc0fffd70a0a16c49499ec9fdac0cd458fcd
Aisha Mohammed <80531561+aisha-rm@users.noreply.github.com>
2021-11-28 01:22:33 +0000

    updated commit

--- sentiment.py
+++ sentiment.py
@@ -8,7 +8,18 @@ import numpy as np
 from nltk.corpus import stopwords
 import pandas as pd
 # nltk.download('stopwords)
+import json
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS
+stopwords = set(STOPWORDS)
+stopwords.add("app")
 
 
 def fetch_review(id):
@@ -26,7 +37,7 @@ def fetch_review(id):
     for i in range(len(results)):
       result = results[i]["content"]
       allResults.append(result)
-    df = pd.Series(allResults, name="content")
+    df = pd.DataFrame(allResults, columns=["review"])
     
     return df
       
@@ -81,6 +92,74 @@ def process_review(text):
     # join the entire word back to string
     return(" ".join(meaningful_words))
 
+
+
+#AISHA
+def sentiment_scores(sentence):
+    """This function calculates the sentiment scores for each review"""
+ 
+    # Create a SentimentIntensityAnalyzer object.
+    sid_obj = SentimentIntensityAnalyzer()
+ 
+    # polarity_scores method of SentimentIntensityAnalyzer
+    # object gives a sentiment dictionary.
+    # which contains pos, neg, neu, and compound scores.
+    sentiment_dict = sid_obj.polarity_scores(sentence)
+ 
+    # decide sentiment as positive, negative and neutral
+    if sentiment_dict['compound'] >= 0.05 :
+        result="positive"
+    elif sentiment_dict['compound'] <= -0.05 :
+        result="negative"
+    else :
+        result="neutral"
+    return result
+
+
+def sentiment_type(df):
+    """This function returns takes a dataframe with reviews of an app, calculates and returns the appropriate sentiment type of each review"""
+    
+    df["Sentiment"] = df["review"].apply(sentiment_scores)
+    return df
+
+
+def sentiment_chart(df):
+  """This function plots a pie chart of the sentiments gotten from an app's reviews"""
+
+  data = df.groupby("Sentiment")["review"].count()
+  data = data.sort_values(ascending = False)
+  pie, ax = plt.subplots(figsize=[10, 10])
+  plt.xticks(rotation='horizontal')
+  explode = (0, 0.1, 0.1)
+  labels = data.keys()
+  fig = plt.pie(x=data, autopct="%.1f%%", shadow=True, labels=labels, explode=explode, pctdistance=0.5)
+  plt.title('App Sentiments')
+  return fig
+
+
+
+#just trying out a single function here where an app ID is passed and it fetches the reviews, calculates the sentiment scores and plot the sentiment types
+#combines the above functions but takes too long to run. Might not be beeded, depends on what you think. Also codes above may need correction.
+
+# def sentiment_chart(id):
+#   """This function plots a pie chart of the sentiments gotten from an app's reviews"""
+  
+#   # get the reviews for the app using the defined function
+#   reviews = fetch_review(id)  
+  
+#   #get the sentiments from the reviews i.e positive, neutral and negative
+#   reviews["Sentiment"] = reviews["review"].apply(sentiment_scores)  
+
+#   #using the dataframe to plot a pie chart of the sentiments
+#   data = reviews.groupby("Sentiment")["review"].count()
+#   data = data.sort_values(ascending = False)
+#   pie, ax = plt.subplots(figsize=[10, 10])
+#   plt.xticks(rotation='horizontal')
+#   explode = (0, 0.1, 0.1)
+#   labels = data.keys()
+#   fig = plt.pie(x=data, autopct="%.1f%%", shadow=True, labels=labels, explode=explode, pctdistance=0.5)
+#   plt.title('App Sentiments')
+#   return fig
 
 
 
