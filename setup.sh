186a96c3a36652d2984dd3e19eed1b6fb353e1e8
sharonibejih <sharonkcee@gmail.com>
2022-01-16 01:03:12 +0100

    deployment

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 mkdir -p ~/.streamlit/
 echo "[theme]
 base='light'
-primaryColor=’#080808’
-secondaryBackgroundColor=’#e6d8a7’
+primaryColor='#080808'
+secondaryBackgroundColor='#e6d8a7'
 textColor='#292103'
-font = ‘sans serif’
+font = 'sans serif'
 [server]
 headless = true
 port = $PORT
